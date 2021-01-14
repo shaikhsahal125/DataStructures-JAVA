3634a2adc24c81c61ee7482e7a9aaee8051c8ed0
shaikhsahal125 <shaikhsahal125@gmail.com>
2021-01-14 14:10:04 -0500

    Queue adding

--- src/implementations/Queue.java
+++ src/implementations/Queue.java
@@ -1,5 +1,39 @@
 package implementations;
 
-public class Queue {
+import interfaces.IQueue;
+import interfaces.ISinglyLinkedList;
+
+public class Queue<T> implements IQueue<T>{
+	private ISinglyLinkedList<T> theList = new SinglyLinkedList<>();
+
+	@Override
+	public void enqueue(T data) {
+		// TODO Auto-generated method stub
+		theList.addLast(data);
+		
+	}
+
+	@Override
+	public T dequeue() {
+		// TODO Auto-generated method stub
+		return theList.removeFirst();
+	}
+
+	@Override
+	public T peek() {
+		// TODO Auto-generated method stub
+		return theList.getFirst();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return theList.isEmpty();
+	}
+	
+	@Override
+	public String toString() {
+		return theList.toString();
+	}
 
 }
